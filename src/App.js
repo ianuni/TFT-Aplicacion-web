690eb52e0b6238e3e35d2e20371cfafbdc907a54
ianuni <ian.castro101@alu.ulpgc.es>
2023-05-17 19:11:36 +0100

    Connection with firebase and basic routing
    Both firebase and routing modules were imported, also firebase is configured with the remote project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    "hola"
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="signin" element={<SignIn/>}/>
+      <Route path="signup" element={<SignUp/>}/>
+    </Routes>
   );
 }
 
