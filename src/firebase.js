d9ef2fa80f197d716d61dc5a3122922b456a54bf
ianuni <ian.castro101@alu.ulpgc.es>
2023-07-13 17:49:27 +0100

    final version

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import {getFirestore} from "firebase/firestore"
 import {getStorage} from "firebase/storage"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyDPdxgj4O9IJD2rX6uTlB2lKFVxUQe9ykk",
+  authDomain: "coinmo-8a9cd.firebaseapp.com",
+  projectId: "coinmo-8a9cd",
+  storageBucket: "coinmo-8a9cd.appspot.com",
+  messagingSenderId: "551885906201",
+  appId: "1:551885906201:web:a382f05134ad958339a588"
 });
 
 export const auth = getAuth(app);
